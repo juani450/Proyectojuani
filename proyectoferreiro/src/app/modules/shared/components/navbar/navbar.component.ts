5acf0fab7fe219e753a51931677abafb0eabb7d3
juani450 <ferjuani@yahoo.com.ar>
2024-08-30 10:57:41 -0300

    Agregamos modo oscuro

--- proyectoferreiro/src/app/modules/shared/components/navbar/navbar.component.ts
+++ proyectoferreiro/src/app/modules/shared/components/navbar/navbar.component.ts
@@ -12,24 +12,52 @@ export class NavbarComponent {
   logueado = true;  //variable booleana para el boton de registro e inicio de sesion
   deslogueado = false;   //variable booleana para el boton de cerrar sesion
 
-constructor(public servicioAuth:AuthService, public servicioRutas:Router){}
+  constructor(public servicioAuth: AuthService, public servicioRutas: Router) { }
 
 
-//cambia los valores delogueado y deslogueado para ocultar los primeros y mostrar el ultimo
-iniciar(){
-  this.logueado = false;
-  this.deslogueado = true
-}
+  //cambia los valores delogueado y deslogueado para ocultar los primeros y mostrar el ultimo
+  iniciar() {
+    this.logueado = false;
+    this.deslogueado = true
+  }
 
-cerrarSesion(){
-  this.deslogueado = false;
-  //metodo cerrar sesion va a eliminar el token actual del usuario
-  //token: estado actual del usuario en el navegador para mamtener la sesion abierta
-  this.servicioAuth.cerrarSesion(); 
+  cerrarSesion() {
+    this.deslogueado = false;
+    //metodo cerrar sesion va a eliminar el token actual del usuario
+    //token: estado actual del usuario en el navegador para mamtener la sesion abierta
+    this.servicioAuth.cerrarSesion();
 
-  this.servicioRutas.navigate(['/']);  //redirigimos a la raiz de la pagina
-  this.logueado = true;
-  
-}
+    this.servicioRutas.navigate(['/']);  //redirigimos a la raiz de la pagina
+    this.logueado = true;
+
+  }
+
+
+
+
+
+  //Funcion para modo oscuro
+  cambiarFondo() {
+    let toggle: HTMLInputElement | null = document.getElementById("toggle") as HTMLInputElement
+    let label_toggle: HTMLElement | null = document.getElementById("label_toggle") as HTMLElement
+
+    if (toggle) {
+      let checked: boolean = toggle.checked;
+      document.body.classList.toggle("dark", checked)
+
+      if(checked){
+        label_toggle!.innerHTML = '<i class="fa-regular fa-sun"></i>'
+      }else{
+
+        label_toggle!.innerHTML = '<i class="fa-regular fa-moon"></i>'
+
+      }
+    }
+  }
+
+
+
+
+
 
 }
